408147ad2a4017ecc82fe8aade406ce9770e4561
fatalst <31542223+fatalst@users.noreply.github.com>
2018-09-02 18:38:28 -0500

    main file created

---
+++ src/main/java/main.java
@@ -0,0 +1,17 @@
+package java;
+import java.util.*;
+import java.util.Scanner;
+
+public class Main {
+
+  public static void main(final String[] args) throws InterruptedException {
+
+  	int rounds = 0; // integer to count the number of rounds
+  	String player1 = "Player 1"; // String holding the name/username of player 1
+  	String player2 = "Player 2"; // String holding the name/username of player 2
+  	boolean game = true; // boolean tracking if the game is currently being played or not (used for looping)
+  	boolean p1_playing = true; // using a boolean to track which player's turn it is (player 1 = true, player 2 = false)
+
+  }
+
+}
