d4f34e8068de93f5ec57067fce361273f2639a36
Megan OToole <motoole4@luc.edu>
2018-09-03 12:45:38 -0500

    Added methods getPlayerOne and getPlayerTwo that receives users desired usernames and stores them in Strings player1 and player2. Loops until user enters a string for their username.

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -12,6 +12,28 @@ public class Main {
   	boolean game = true; // boolean tracking if the game is currently being played or not (used for looping)
   	boolean p1_playing = true; // using a boolean to track which player's turn it is (player 1 = true, player 2 = false)
 
-  }
+	player1 = Main.getPlayerOne(player1); //sets player1 to the first players specified username
+	player2 = Main.getPlayerTwo(player2); //sets player2 to the second players specified username
+}
+	private static String getPlayerOne(String player1){
+		Scanner sc = new Scanner(System.in); //opens scanner for user input
+		do{
+			System.out.println("Player One, Please enter your username.");
+			player1 = sc.nextLine(); //sets input to player1
+		}while(player1.isEmpty()); //loops until user enters a username for player
+		//could change to allow only a certain # of retry attempts before closing scanner and using
+        //default name
+		return player1;
+	}
 
+	private static String getPlayerTwo(String player2){
+		Scanner sc = new Scanner(System.in); //opens scanner for user input
+		do{
+			System.out.println("Player Two, Please enter your username.");
+			player2 = sc.nextLine(); //sets input to player2
+		}while(player2.isEmpty()); //loops until user enters a username for player2
+		//could change to allow only a certain # of retry attempts before closing scanner and using
+        //default name
+		return player2;
+	}
 }
--- src/main/java/ui.java
+++ src/main/java/ui.java
@@ -2,7 +2,5 @@ package java;
 import java.util.*;
 
 public class UI{
-
-	
 	
 }
