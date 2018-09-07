35cd38d301b3d747b43a3e48f73d096bc901842b
Megan OToole <motoole4@luc.edu>
2018-09-06 23:14:03 -0500

    Added to UI: changePlayer() which changes player marker after every turn getPlayerMarker() helps keep track of whose turn it is w/o changing the players turn placeMarker() places players marker on board at desired coordinates
    Added to Main:
    I changed a few things so I commented them out so you could take a look at them
    getRow() to get and return users x coordinate choice
    getCol() to get and return users y coordinate choice
    
    ***There is one known bug, the row coordinate isn't placed on board correctly

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,33 +4,53 @@ import java.util.*;
 import java.util.Scanner;
 
 public class Main {
-
   public static void main(final String[] args) throws InterruptedException {
+    UI ui = new UI(); // creates new UI object
 
     int rounds = 0; // integer to count the number of rounds
+
+    // player information
     String player1 = "Player 1"; // String holding the name/username of player 1
+    player1 = Main.getPlayerOne(player1); // sets player1 to the first players specified username
     String player2 = "Player 2"; // String holding the name/username of player 2
+    player2 = Main.getPlayerTwo(player2); // sets player2 to the second players specified username
     boolean game =
         true; // boolean tracking if the game is currently being played or not (used for looping)
     boolean p1_playing =
         true; // using a boolean to track which player's turn it is (player 1 = true, player 2 =
     // false)
-
-    player1 = Main.getPlayerOne(player1); // sets player1 to the first players specified username
-    player2 = Main.getPlayerTwo(player2); // sets player2 to the second players specified username
-
-    UI ui = new UI(); // creates new UI object
-    ui.createBoard(); // fills the board with blanks
-    ui.printBoard(); // initially prints the board
-
     System.out.println("");
     System.out.println(player1 + ", you are Xs.");
     System.out.println(player2 + ", you are Os.");
 
     Main.takeInput(player1, player2, p1_playing);
 
-  }
+    /*
+    // start game
+    ui.createBoard(); // fills the board with blanks
+    ui.printBoard(); // initially prints the board
 
+    do {
+      char marker = ui.getPlayerMarker(); // keep track of whose turn
+
+      int row =
+          Main.getRow(player1, player2, marker); // it puts row 0 at the top, instead of bottom
+      // like an actually coord plane... i'll fix that later
+      int col = Main.getCol(player1, player2, marker);
+      System.out.println(
+          "Your coordinates are: " + row + ", " + col); // mostly for testing but could keep in
+
+      ui.placeMark(row, col); // places mark on the board
+      ui.printBoard(); // prints board with added mark for current update of game
+
+      // TODO check for winner, if winner game == false to quit game
+
+      ui.changePlayer(); // alternate turn and player mark
+    } while (game == true);
+    // TODO: option for rematch
+    */
+
+  }
   private static String getPlayerOne(String player1) {
     Scanner sc = new Scanner(System.in); // opens scanner for user input
     int count = 0; // counts how many time user enters null username
@@ -57,26 +77,56 @@ public class Main {
     return "Player 2"; // sets username to default username
   }
 
-  private static void takeInput(String player1, String player2, boolean p1_playing){
-  	System.out.println("");
-  	Scanner sc = new Scanner(System.in); // initializes the Scanner
-  	String name;
+  public static void takeInput(String player1, String player2, boolean p1_playing) {
+    System.out.println("");
+    Scanner sc = new Scanner(System.in); // initializes the Scanner
+    String name;
 
-  	if (p1_playing == true){
-  		name = player1;
-  	} else{
-  		name = player2;
-  	}
+    if (p1_playing == true) {
+      name = player1;
+    } else {
+      name = player2;
+    }
 
-  	System.out.println(name + ", it is your turn.");
-  	System.out.println("");
-  	System.out.print("Choose a coordinate on the x axis: ");
-  	int x = Integer.parseInt(sc.nextLine()); // sets x coordinate to player input
-  	System.out.print("Choose a coordinate on the y axis: ");
-  	int y = Integer.parseInt(sc.nextLine()); // sets y coordinate to player input
+    System.out.println(name + ", it is your turn.");
+    System.out.println("");
+    System.out.print("Choose a coordinate on the x axis: ");
+    int x = Integer.parseInt(sc.nextLine()); // sets x coordinate to player input
+    System.out.print("Choose a coordinate on the y axis: ");
+    int y = Integer.parseInt(sc.nextLine()); // sets y coordinate to player input
 
-  	System.out.println("X: " + x + " / Y: " + y); // for testing purposes
-
+    System.out.println("X: " + x + " / Y: " + y); // for testing purposes
   }
+
+  /*
+  I split up gewtting the row and col coordinates so we could use the return values in the main
+  method I couldn't figure out ho to do it from the takeInput method. Feel free to deletes these
+  if you know how/ had something else in mind
+
+  public static int getRow(String player1, String player2, char marker) {
+    System.out.println("");
+    Scanner sc = new Scanner(System.in);
+    String name;
+    if (marker == 'X') {
+      name = player1;
+    } else {
+      name = player2;
+    }
+    System.out.println(name + ", it is your turn.\n");
+    System.out.print("Choose a coordinate on the x axis: \n");
+    int row = Integer.parseInt(sc.nextLine()); // sets x coordinate to player input
+
+    return row;
+  }
+
+  public static int getCol(String player1, String player2, char marker) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("");
+    System.out.print("Choose a coordinate on the y axis: \n");
+    int col = Integer.parseInt(sc.nextLine()); // sets y coordinate to player input
+
+    return col;
+  }
+  */
 
 }
--- src/main/java/UI.java
+++ src/main/java/UI.java
@@ -5,9 +5,12 @@ import java.util.*;
 public class UI {
 
   private char[][] gameBoard;
+  private char currentPlayerMark;
 
   public UI() {
     gameBoard = new char[3][3]; // declare 2d array gameBoard of size 3x3
+    currentPlayerMark = 'X'; // makes player1 X
+    createBoard();
   }
 
   public void createBoard() {
@@ -19,16 +22,50 @@ public class UI {
   }
 
   public void printBoard() {
-  	System.out.println("");
+    System.out.println("");
     System.out.println("-------------"); // creates top boarder
     for (int i = 0; i < 3; i++) {
       System.out.print("| "); // creates left boarder
       for (int j = 0; j < 3; j++) {
-        System.out.print(
-            gameBoard[i][j] + " | "); // creates boarders between columns + right boarder
+        // creates boarders between columns + right boarder
+        System.out.print(gameBoard[i][j] + " | ");
       }
       System.out.println();
       System.out.println("-------------"); // creates boarders between rows + bottom boarder
     }
+  }
+
+  public void changePlayer() {
+    // changes to currentPlayerMark after every turn
+    if (currentPlayerMark == 'X') {
+      currentPlayerMark = 'O';
+    } else {
+      currentPlayerMark = 'X';
+    }
+  }
+
+  public char getPlayerMarker() {
+    // to help keep track of whose turn it is, gets CurrentPlayerMark w/o changing anything
+    char marker = ' ';
+    if (currentPlayerMark == 'X') {
+      marker = 'X';
+    } else {
+      marker = 'O';
+    }
+    return marker;
+  }
+
+  public boolean placeMark(int row, int col) {
+    // places the playing marks in their desired coordinates
+    // could implement another try is row/col is out of bounds or spot is taken
+    if ((row >= 0) && (row < 3)) { // checks to make sure row number entered is in ranged
+      if ((col >= 0) && (col < 3)) { // checks to make sure col number entered is in ranged
+        if (gameBoard[row][col] == ' ') { // check to make sure spot is open
+          gameBoard[row][col] = currentPlayerMark; // sets mark to coordinate location
+          return true;
+        }
+      }
+    }
+    return false;
   }
 }
