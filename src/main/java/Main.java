bc771e04d1b865a7b7bbb04a4511fc31141b3050
fatalst <31542223+fatalst@users.noreply.github.com>
2018-09-10 12:57:05 -0500

    formatting & almost finished checkEnding()

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,144 +5,155 @@ import java.util.Scanner;
 
 public class Main {
 
-    private static int rounds; // integer to count the number of rounds
-    private static boolean game; // boolean tracking if the game is currently being played or not(used for looping)
-    private static boolean p1_playing;//using a boolean to track which player's turn it is(player 1 = true, player 2 = false)
+	private static int rounds; // integer to count the number of rounds
+	private static boolean game; // boolean tracking if the game is currently being played or not(used for looping)
+	private static boolean p1_playing;//using a boolean to track which player's turn it is(player 1 = true, player 2 = false)
 
-    private static String player1;
-    private static String player2;
-    private static UI ui = new UI(); // creates new UI object
+	private static String player1;
+	private static String player2;
+	private static UI ui = new UI(); // creates new UI object
 
-    public static void main(final String[] args) throws InterruptedException {
-      Scanner sc = new Scanner(System.in); // opens scanner for user input
-      Main.getPlayerInfo();
-      Main.startGame();
-      boolean validResponse;
-      boolean rematch = true;
+	public static void main(final String[] args) throws InterruptedException {
+		Scanner sc = new Scanner(System.in); // opens scanner for user input
+		Main.getPlayerInfo();
+		Main.startGame();
+		boolean validResponse;
+		boolean rematch = true;
 
-      do {
-        game = true;
-        do {
-          char marker = ui.getPlayerMarker(); // keep track of whose turn
+		do {
+			game = true;
+			do {
+				char marker = ui.getPlayerMarker(); // keep track of whose turn
 
-          int row = Main.getRow(player1, player2, marker);
-          int col = Main.getCol(player1, player2, marker);
-          System.out.println(
-                  "Your coordinates are: " + row + ", " + col); // mostly for testing but could keep in
+				int row = Main.getRow(player1, player2, marker);
+				int col = Main.getCol(player1, player2, marker);
+				System.out.println(
+					"Your coordinates are: " + row + ", " + col); // mostly for testing but could keep in
 
-          validResponse = ui.placeMark(row, col); // returns if the response is valid or not; if valid, places mark on the board
+				validResponse = ui.placeMark(row, col); // returns if the response is valid or not; if valid, places mark on the board
 
-          // checking if the input is out of bounds
-          if (validResponse == false) {
-            System.out.println("That is an invalid set of coordinates.  Please try again.");
-            continue;
-          }
+				// checking if the input is out of bounds
+				if (validResponse == false) {
+					System.out.println("That is an invalid set of coordinates.  Please try again.");
+					continue;
+				}
 
-          ui.printBoard(); // prints board with added mark for current update of game
+				ui.printBoard(); // prints board with added mark for current update of game
 
-          rounds++;
+				rounds++;
 
-          // TODO check for winner, if winner game == false to quit game
-          String winner = "";
-          if (ui.checkEnding() > 0) {
-            game = false;
-            if (ui.checkEnding() == 1) {
-              System.out.println(player1 + " wins!");
-            } else {
-              System.out.println(player2 + " wins!");
-            }
-          }
+				// TODO check for winner, if winner game == false to quit game
+				// String winner = "";
+				// if (ui.checkEnding() > 0) {
+				// 	game = false;
+				// 	if (ui.checkEnding() == 1) {
+				// 		System.out.println(player1 + " wins!");
+				// 	} else {
+				// 		System.out.println(player2 + " wins!");
+				// 	}
+				// }
 
-          // assuming wrong answers are impossible AND if someone wins, the game ends -- ends the game on a draw.
-          if (rounds == 9) {
-            System.out.println("");
-            System.out.println("Draw!");
-            game = false;
-          }
+				if(rounds == 5) {
+					if(ui.checkEnding() == true) {
+						if(ui.getPlayerMarker() == 'X') {
+							System.out.println(player1 + " wins!");
+							game = false;
+						} else if(ui.getPlayerMarker() == 'O') {
+							System.out.println(player2 + " wins!");
+							game = false;
+						}
+					}
+				// assuming wrong answers are impossible AND if someone wins, the game ends -- ends the game on a draw.
+				} else if (rounds == 9) {
+					System.out.println("");
+					System.out.println("Draw!");
+					game = false;
+				}
 
-          ui.changePlayer(); // alternate turn and player mark
-        } while (game == true);
+				ui.changePlayer(); // alternate turn and player mark
+			} while (game == true);
 
-        System.out.println("Play again? (Y/N)");
-        String response = sc.nextLine();
-        if (response.equalsIgnoreCase("Y")) {
-          rematch = true;
-          ui.reset();
-          Main.startGame();
-        }else rematch = false;
-      }while(rematch == true);
-    }
-    private static void startGame(){
-      rounds = 0; // integer to count the number of rounds
+			System.out.println("Play again? (Y/N)");
+			String response = sc.nextLine();
+			if (response.equalsIgnoreCase("Y")) {
+				rematch = true;
+				ui.reset();
+				Main.startGame();
+			} else rematch = false;
+		} while(rematch == true);
+	}
 
-      // start game
-      ui.createBoard(); // fills the board with blanks
-      ui.printBoard(); // initially prints the board
-    }
-    private static void getPlayerInfo(){
-      //created its own method so the players wouldn't have to retype username for rematch
-      // player information
-      player1 = "Player 1"; // String holding the name/username of player 1
-      player1 = Main.getPlayerOne(player1); // sets player1 to the first players specified username
-      player2 = "Player 2"; // String holding the name/username of player 2
-      player2 = Main.getPlayerTwo(player2); // sets player2 to the second players specified username
-      game =
-              true; // boolean tracking if the game is currently being played or not (used for looping)
-      p1_playing =
-              true; // using a boolean to track which player's turn it is (player 1 = true, player 2 =
-      // false)
-      System.out.println("");
-      System.out.println(player1 + ", you are Xs.");
-      System.out.println(player2 + ", you are Os.");
-    }
+	private static void startGame() {
+		rounds = 0; // integer to count the number of rounds
 
-  private static String getPlayerOne(String player1) {
-    Scanner sc = new Scanner(System.in); // opens scanner for user input
-    int count = 0; // counts how many time user enters null username
-    do {
-      System.out.print("Player One, please enter your username: ");
-      player1 = sc.nextLine(); // sets input to player1
-      count++;
-    } while (player1.isEmpty() && count < 3); // loops 3 times for user to enter a username
+		// start game
+		ui.createBoard(); // fills the board with blanks
+		ui.printBoard(); // initially prints the board
+	}
+	private static void getPlayerInfo() {
+		//created its own method so the players wouldn't have to retype username for rematch
+		// player information
+		player1 = "Player 1"; // String holding the name/username of player 1
+		player1 = Main.getPlayerOne(player1); // sets player1 to the first players specified username
+		player2 = "Player 2"; // String holding the name/username of player 2
+		player2 = Main.getPlayerTwo(player2); // sets player2 to the second players specified username
+		game =
+			true; // boolean tracking if the game is currently being played or not (used for looping)
+		p1_playing =
+			true; // using a boolean to track which player's turn it is (player 1 = true, player 2 =
+		// false)
+		System.out.println("");
+		System.out.println(player1 + ", you are Xs.");
+		System.out.println(player2 + ", you are Os.");
+	}
 
-    if (!player1.isEmpty()) return player1; // sets username to given input
-    else return "Player 1"; // sets username to default username
-  }
+	private static String getPlayerOne(String player1) {
+		Scanner sc = new Scanner(System.in); // opens scanner for user input
+		int count = 0; // counts how many time user enters null username
+		do {
+			System.out.print("Player One, please enter your username: ");
+			player1 = sc.nextLine(); // sets input to player1
+			count++;
+		} while (player1.isEmpty() && count < 3); // loops 3 times for user to enter a username
 
-  private static String getPlayerTwo(String player2) {
-    Scanner sc = new Scanner(System.in); // opens scanner for user input
-    int count = 0; // counts how many time user enters null username
-    do {
-      System.out.print("Player Two, please enter your username: ");
-      player2 = sc.nextLine(); // sets input to player2
-      count++;
-    } while (player2.isEmpty() && count < 3); // loops 3 time for user to enter a username
+		if (!player1.isEmpty()) return player1; // sets username to given input
+		else return "Player 1"; // sets username to default username
+	}
 
-    if (!player2.isEmpty()) return player2; // sets username to given input
-    return "Player 2"; // sets username to default username
-  }
+	private static String getPlayerTwo(String player2) {
+		Scanner sc = new Scanner(System.in); // opens scanner for user input
+		int count = 0; // counts how many time user enters null username
+		do {
+			System.out.print("Player Two, please enter your username: ");
+			player2 = sc.nextLine(); // sets input to player2
+			count++;
+		} while (player2.isEmpty() && count < 3); // loops 3 time for user to enter a username
 
-  public static int getRow(String player1, String player2, char marker) {
-    System.out.println("");
-    Scanner sc = new Scanner(System.in);
-    String name;
-    if (marker == 'X') {
-      name = player1;
-    } else {
-      name = player2;
-    }
-    System.out.println(name + ", it is your turn.\n");
-    System.out.print("Choose a coordinate on the x axis: \n");
-    int row = Integer.parseInt(sc.nextLine()); // sets x coordinate to player input
+		if (!player2.isEmpty()) return player2; // sets username to given input
+		return "Player 2"; // sets username to default username
+	}
 
-    return row;
-  }
-  public static int getCol(String player1, String player2, char marker) {
-    Scanner sc = new Scanner(System.in);
-    System.out.println("");
-    System.out.print("Choose a coordinate on the y axis: \n");
-    int col = Integer.parseInt(sc.nextLine()); // sets y coordinate to player input
+	public static int getRow(String player1, String player2, char marker) {
+		System.out.println("");
+		Scanner sc = new Scanner(System.in);
+		String name;
+		if (marker == 'X') {
+			name = player1;
+		} else {
+			name = player2;
+		}
+		System.out.println(name + ", it is your turn.\n");
+		System.out.print("Choose a coordinate on the x axis: \n");
+		int row = Integer.parseInt(sc.nextLine()); // sets x coordinate to player input
 
-    return col;
-  }
+		return row;
+	}
+	public static int getCol(String player1, String player2, char marker) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("");
+		System.out.print("Choose a coordinate on the y axis: \n");
+		int col = Integer.parseInt(sc.nextLine()); // sets y coordinate to player input
+
+		return col;
+	}
 }
--- src/main/java/UI.java
+++ src/main/java/UI.java
@@ -4,127 +4,116 @@ import java.util.*;
 
 public class UI {
 
-  private char[][] gameBoard;
-  private char currentPlayerMark;
+	private char[][] gameBoard;
+	private char currentPlayerMark;
 
-  public UI() {
-    gameBoard = new char[3][3]; // declare 2d array gameBoard of size 3x3
-    currentPlayerMark = 'X'; // makes player1 X
-    createBoard();
-  }
+	public UI() {
+		gameBoard = new char[3][3]; // declare 2d array gameBoard of size 3x3
+		currentPlayerMark = 'X'; // makes player1 X
+		createBoard();
+	}
 
-  public void reset() {
-    currentPlayerMark = 'X'; // makes player1 X
-  }
+	public void reset() {
+		currentPlayerMark = 'X'; // makes player1 X
+	}
 
-  public void createBoard() {
-    for (int i = 2; i >= 0; i--) { // loops through rows
-      for (int j = 0; j < 3; j++) { // loops through columns
-        gameBoard[i][j] = ' '; // fills board with blank spaces
-      }
-    }
-  }
+	public void createBoard() {
+		for (int i = 2; i >= 0; i--) { // loops through rows
+			for (int j = 0; j < 3; j++) { // loops through columns
+				gameBoard[i][j] = ' '; // fills board with blank spaces
+			}
+		}
+	}
 
-  // added visual coordinates to make things easier for user input.
-  public void printBoard() {
-    int num = 2;
-    System.out.println("");
-    System.out.println(
-        ". 0 . . 1 . . 2"); // adding coordinates to the top. '.'s are placeholders for whitespace.
-    System.out.println(". -------------"); // creates top boarder
-    for (int i = 2; i >= 0; i--) {
-      System.out.print(num + " | "); // creates left boarder
-      num--; // adding coordinates to the side
-      for (int j = 0; j < 3; j++) {
-        // creates boarders between columns + right boarder
-        System.out.print(gameBoard[i][j] + " | ");
-      }
-      System.out.println();
-      System.out.println(". -------------"); // creates boarders between rows + bottom boarder
-    }
-  }
+	// added visual coordinates to make things easier for user input.
+	public void printBoard() {
+		int num = 2;
+		System.out.println("");
+		System.out.println(
+			". 0 . . 1 . . 2"); // adding coordinates to the top. '.'s are placeholders for whitespace.
+		System.out.println(". -------------"); // creates top boarder
+		for (int i = 2; i >= 0; i--) {
+			System.out.print(num + " | "); // creates left boarder
+			num--; // adding coordinates to the side
+			for (int j = 0; j < 3; j++) {
+				// creates boarders between columns + right boarder
+				System.out.print(gameBoard[i][j] + " | ");
+			}
+			System.out.println();
+			System.out.println(". -------------"); // creates boarders between rows + bottom boarder
+		}
+	}
 
-  public void changePlayer() {
-    // changes to currentPlayerMark after every turn
-    if (currentPlayerMark == 'X') {
-      currentPlayerMark = 'O';
-    } else {
-      currentPlayerMark = 'X';
-    }
-  }
+	public void changePlayer() {
+		// changes to currentPlayerMark after every turn
+		if (currentPlayerMark == 'X') {
+			currentPlayerMark = 'O';
+		} else {
+			currentPlayerMark = 'X';
+		}
+	}
 
-  public char getPlayerMarker() {
-    // to help keep track of whose turn it is, gets CurrentPlayerMark w/o changing anything
-    char marker = ' ';
-    if (currentPlayerMark == 'X') {
-      marker = 'X';
-    } else {
-      marker = 'O';
-    }
-    return marker;
-  }
+	public char getPlayerMarker() {
+		// to help keep track of whose turn it is, gets CurrentPlayerMark w/o changing anything
+		char marker = ' ';
+		if (currentPlayerMark == 'X') {
+			marker = 'X';
+		} else {
+			marker = 'O';
+		}
+		return marker;
+	}
 
-  public boolean placeMark(int row, int col) {
-    // places the playing marks in their desired coordinates
-    // could implement another try is row/col is out of bounds or spot is taken
-    if ((row >= 0) && (row < 3)) { // checks to make sure row number entered is in ranged
-      if ((col >= 0) && (col < 3)) { // checks to make sure col number entered is in ranged
-        if (gameBoard[row][col] == ' ') { // check to make sure spot is open
-          gameBoard[row][col] = currentPlayerMark; // sets mark to coordinate location
-          return true;
-        }
-      }
-    }
-    return false;
-  }
-  // checking the endings. unfinished.
-  public int checkEnding(){
-    int xCount = 0;
-    int oCount = 0;
+	public boolean placeMark(int row, int col) {
+		// places the playing marks in their desired coordinates
+		// could implement another try is row/col is out of bounds or spot is taken
+		if ((row >= 0) && (row < 3)) { // checks to make sure row number entered is in ranged
+			if ((col >= 0) && (col < 3)) { // checks to make sure col number entered is in ranged
+				if (gameBoard[row][col] == ' ') { // check to make sure spot is open
+					gameBoard[row][col] = currentPlayerMark; // sets mark to coordinate location
+					return true;
+				}
+			}
+		}
+		return false;
+	}
 
-    // checking rows
-    for(int i = 0; i < 3; i++){
-      for (int j = 0; j < 3; j++) {
-        if(gameBoard[i][j] == 'X'){
-          xCount++;
-        } else if (gameBoard[i][j] == 'O'){
-          oCount++;
-        }
-      }
-      if(xCount == 3 || oCount == 3){
-        return winnerPlayer(xCount, oCount);
-      }
-    }
+	// checking for a winner
+	public boolean checkEnding() {
 
-    xCount = 0;
-    oCount = 0;
+		char mark = currentPlayerMark;
 
-    // checking columns
-    for(int i = 0; i < 3; i++){
-      for (int j = 0; j < 3; j++) {
-        if(gameBoard[j][i] == 'X'){
-          xCount++;
-        } else if (gameBoard[j][i] == 'O'){
-          oCount++;
-        }
-      }
-      if(xCount == 3 || oCount == 3){
-        return winnerPlayer(xCount, oCount);
-      }
-    }
+		if(gameBoard[0][0] == mark && gameBoard[0][1] == mark && gameBoard[0][2] == mark) { //checking top row
+			return true;
+		} else if(gameBoard[1][0] == mark && gameBoard[1][1] == mark && gameBoard[1][2] == mark) { // checking middle row
+			return true;
+		} else if(gameBoard[2][0] == mark && gameBoard[2][1] == mark && gameBoard[2][2] == mark) { // checking bottom row
+			return true;
+		} else if(gameBoard[0][0] == mark && gameBoard[1][0] == mark && gameBoard[2][0] == mark) { // checking first column
+			return true;
+		} else if(gameBoard[0][1] == mark && gameBoard[1][1] == mark && gameBoard[2][1] == mark) { // checking middle column
+			return true;
+		} else if(gameBoard[0][2] == mark && gameBoard[1][2] == mark && gameBoard[2][2] == mark) { // checking last column
+			return true;
+		} else if(gameBoard[2][0] == mark && gameBoard[1][1] == mark && gameBoard[0][2] == mark) { // checking left to right diagonal
+			return true;
+		} else if(gameBoard[2][2] == mark && gameBoard[1][1] == mark && gameBoard[0][0] == mark) { // checking first column
+			return true;
+		} else{
+			System.out.println("FALSE");
+			return false;
+		}
 
-    return 0;
+	}
 
-  }
-
-  public int winnerPlayer(int xCount, int oCount){
-    if(xCount == 3){
-      return 1; // return that player 1 has won
-    } else if(oCount == 3){
-      return 2; // return that player 2 has won
-    } else{
-      return 0;
-    }
-  }
+	public int winnerPlayer(int xCount, int oCount) {
+		if(xCount == 3) {
+			return 1; // return that player 1 has won
+		} else if(oCount == 3) {
+			return 2; // return that player 2 has won
+		} else {
+			return 0;
+		}
+	}
 
 }
